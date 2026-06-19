14a31be997991b326019b599a69ffaaa6f73f6a4
Anton Dettki <caspardett@gmail.com>
2026-06-19 18:45:08 +0200

    feat(FE): style index page

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,16 +1,65 @@
 import { Button } from '#/components/ui/button'
-import { createFileRoute } from '@tanstack/react-router'
+import { Link, createFileRoute } from '@tanstack/react-router'
 import { useNavigate } from '@tanstack/react-router'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { ArrowRightIcon } from 'lucide-react'
+
 export const Route = createFileRoute('/')({ component: App })
 
 function App() {
   const navigate = useNavigate()
   return (
   <div>
-    <h1>Sustaind Rechtskataster</h1>
+    <div className="flex flex-row flex-wrap justify-center gap-4 my-15">
+    <Card className="w-full max-w-md flex-1 min-w-[280px]">
+      <CardHeader>
+        <CardTitle>Rechtsnormen</CardTitle>
+        <CardDescription>
+          Verwalte deine Rechtsnormen.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+        </p>
+      </CardContent>
+      <CardFooter>
+        <Button variant="outline" size="sm" className="w-full" asChild>
+          <Link to="/legal-acts">
+            Zu den Rechtsnormen <ArrowRightIcon />
+          </Link>
+        </Button>
+      </CardFooter>
+    </Card>
 
-    <Button onClick={() => navigate({ to: '/legal-acts' })}> Go to legal acts</Button>
-    <Button onClick={() => navigate({ to: '/obligations' })}> Go to obligations</Button>
+    <Card className="w-full max-w-md flex-1 min-w-[280px]">
+      <CardHeader>
+        <CardTitle>Pflichten</CardTitle>
+        <CardDescription>
+          Verwalte deine Pflichten.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+        </p>
+      </CardContent>
+      <CardFooter>
+        <Button variant="outline" size="sm" className="w-full" asChild>
+          <Link to="/obligations" search={{ short: undefined }}>
+            Zu den Pflichten <ArrowRightIcon />
+          </Link>
+        </Button>
+      </CardFooter>
+    </Card>
+    </div>
   </div>
   )
 }
